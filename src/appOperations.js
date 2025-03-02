ad123c21a12ac2a65a062a28eab039d77985fe86
PathumFe <pathummadhurang@gmail.com>
2025-03-03 00:58:00 +0530

    second func

--- src/appOperations.js
+++ src/appOperations.js
@@ -3,10 +3,11 @@ function multiply(num1, num2){
     return num1*num2
 }
 
-// function add(num1, num2){
-//     return num1+num2;
-// }
+function add(num1, num2){
+    return num1+num2;
+}
 
 module.exports = {
     multiply,
+    add
 }
