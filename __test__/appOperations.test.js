ab8ad49dcda0a8b4f893f14dec8509d00745c1bf
PathumFe <pathummadhurang@gmail.com>
2025-03-03 01:04:37 +0530

    rewwrite test cases

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,5 +1,8 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication property of zero", () =>{
-    expect(appOpeartions(5, 0)).toBe(0);
+    expect(appOpeartions.multiply(5, 0)).toBe(0);
+})
+test("multiplication property of zero", () =>{
+    expect(appOpeartions.add(5, 10)).toBe(15);
 })
