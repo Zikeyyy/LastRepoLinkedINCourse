763efcd762f21d919e8cd2d966cdab4377f5e940
PathumFe <pathummadhurang@gmail.com>
2025-03-03 01:08:02 +0530

    debuf test cases

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -1,8 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication property of zero", () =>{
-    expect(appOpeartions.multiply(5, 0)).toBe(0);
-})
-test("multiplication property of zero", () =>{
-    expect(appOpeartions.add(5, 10)).toBe(15);
-})
+    expect(appOperations.multiply(5, 0)).toBe(0);
+});
+
+test("adding property of zero", () =>{
+    expect(appOperations.add(5, 5)).toBe(10);
+});
