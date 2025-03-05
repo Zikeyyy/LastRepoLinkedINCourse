03e8ee439f8f2431bb6c8253e2bfc5eb0db35b8f
PathumFe <pathummadhurang@gmail.com>
2025-03-05 14:03:22 +0530

    new func

--- src/appOperations.js
+++ src/appOperations.js
@@ -1,9 +1,13 @@
 function multiply(num1, num2){
-    return num1*num2
+    return num1*num2;
 }
 
 function add(num1, num2){
     return num1+num2;
+}
+
+function devide(num1, num2){
+    return num1/num2;
 }
 
 function subtract(num1, num2){
@@ -13,5 +17,6 @@ function subtract(num1, num2){
 module.exports = {
     multiply,
     add,
-    subtract
+    subtract,
+    devide
 }
