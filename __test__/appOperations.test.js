d13219c02e0bd4e538af694b47a740902087d61d
PathumFe <pathummadhurang@gmail.com>
2025-03-05 14:03:22 +0530

    new func

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -8,6 +8,12 @@ test("add property of two nums", () =>{
     expect(appOperations.add(5, 10)).toBe(15);
 })
 
+
 test("sub property of two nums", () =>{
     expect(appOperations.subtract(55, 40)).toBe(15);
 })
+
+test("devide property of two nums", () =>{
+    expect(appOperations.devide(165, 11)).toBe(15);
+})
+
