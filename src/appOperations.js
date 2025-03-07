b42b67b9bd3082631ef0266df12f0041edf75a51
PathumFe <pathummadhurang@gmail.com>
2025-03-08 00:13:23 +0530

    add pkg

--- src/appOperations.js
+++ src/appOperations.js
@@ -3,8 +3,11 @@ function multiply(num1, num2){
 }
 
 function add(num1, num2){
+    const intNum1 = parseInt(num1, 10);
+    const intNum2 = parseInt(num2, 10);
     return num1+num2;
 }
+//ss
 
 function devide(num1, num2){
     return num1/num2;
